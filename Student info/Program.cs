b88ed9d1c8afda0ed0d742f6ae1f7349e3a1b8d5
reyes221093 <102719907+reyes221093@users.noreply.github.com>
2022-05-20 14:16:55 +0800

    Add project files.

---
+++ Student info/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Student_info
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
+namespace StudentManespace
+{
+    internal class StudentNameclass
+    {
+        public String StudentNameVar { get; set; }
+
+        public StudentNameclass()
+        {
+
+        }
+
+        public StudentNameclass(string studentNameVar)
+        {
+            this.StudentNameVar = studentNameVar;
+        }
+
+    }
+}
